6c54d33607567082df34e5d22ecc065633c72039
kangseulmi <tmfal5023@naver.com>
2021-02-13 18:51:41 +0900

    server 진짜 최종

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,23 @@
-let auth = (req, res, next) =>{
+const {User} = require("../models/User");
 
+let auth = (req, res, next) =>{
+    //인증처리하는 곳
+
+    //클리이언트 쿠키에서 토큰 가져오기
+    let token = req.cookies.x_auth;
+
+    //토큰 복호화한 후 유저 찾기
+    User.findByToken(token, (err, user)=>{
+        if(err) throw err;
+        if(!user) return res.json({isAuth : false, error : true}) 
+
+        req.token = token;
+        req.user = user;
+        next();
+    })
+    //유저 있음 인증 오케이
+
+    //유저 없으면 인증 no
 
 }
 
--- models/User.js
+++ models/User.js
@@ -2,6 +2,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;//자릿수
+const jwt = require('jasonwebtoken');
+
 const userSchema = mongoose.Schema({
     name:{
         type : String,
@@ -58,6 +60,41 @@ userSchema.pre('save', function(next){
     }
 })
 
+//user method만들기
+userSchema.methods.compardPassword = function(plainPassword, cb){
+    //비밀번호 비교할 때
+    //plain password 12345(암호화해서 ->와 비교)  암호화된 비밀번호 hash값(복호화 불가)
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err),
+            cb(null, isMatch)
+    })
+}
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+
+    //jsonwebtoken 이용해 토큰 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    //토큰을 decode
+    jwt.verfy(token,'secretToken', function(err, decoded){
+        //유저 아이디를 이용해서 유저를 찾은 후에
+        //클라이언트에서 가져온 token과 db에 보관된 토큰이 일치하는지 확인
+        user.findOne({"_id":decoded, "token": token }, function(err, user){
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
 //model로 schema감싸기 (model이름, schema이름)
 const User = mongoose.model('User',userSchema)
 
