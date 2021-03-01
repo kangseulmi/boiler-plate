902cf9700449b9f43f996d6f2e81f987d5d9d624
kangseulmi <tmfal5023@naver.com>
2021-03-01 20:03:48 +0900

    auth.js에러 고침 -> import는 ''(작은따움표)

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { auth } from " ../_actions/user_action";
+import { auth } from '../_actions/user_action';
 
 export default function (SpecificComponent, option, adminRoute = null) {
     //backend에 request를 날려서 현재 상태 가져오기
